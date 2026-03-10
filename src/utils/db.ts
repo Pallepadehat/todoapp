a8ef2aed1fc86559fd0d9ec499e3739e1961ae86
Patrick Jakobsen <patrick.jakobsen@icloud.com>
2026-03-10 10:33:01 +0100

    Init db

---
+++ src/utils/db.ts
@@ -0,0 +1,12 @@
+import schema from "@/instant.schema";
+import { init } from "@instantdb/react-native";
+
+const APP_ID = process.env.EXPO_PUBLIC_INSTANT_APP_ID;
+
+if (!APP_ID) {
+  throw new Error("Missing app id");
+}
+
+const db = init({ appId: APP_ID, schema: schema });
+
+export default db;
